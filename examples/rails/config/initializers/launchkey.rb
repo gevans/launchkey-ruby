fe08eb38a16ed5b83a6c79cc2c43e13c0b021526
Gabe Evans <gabe@ga.be>
2013-11-18 05:49:05 -0800

    Update keypair and config for development

--- examples/rails/config/initializers/launchkey.rb
+++ examples/rails/config/initializers/launchkey.rb
@@ -8,7 +8,7 @@ LaunchKey.configure do |config|
   config.domain     = ENV['LAUNCHKEY_DOMAIN']
   config.app_key    = ENV['LAUNCHKEY_APP_KEY']
   config.secret_key = ENV['LAUNCHKEY_SECRET_KEY']
-  config.keypair    = File.read Rails.root.join('config/launchkey_keypair.pem')
+  config.keypair    = File.open Rails.root.join('config/launchkey_keypair.pem')
   config.passphrase = ENV['LAUNCHKEY_KEYPAIR_PASSPHRASE']
 end
 
